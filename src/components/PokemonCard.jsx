9a862c2aa46cc271eef8c29752fb131cd429fa5a
cedric chemin <cedric.chemin33@gmail.com>
2023-12-12 23:02:49 +0100

    quete 5

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 
 function PokemonCard(props) {
     console.log(props);
@@ -14,6 +15,21 @@ function PokemonCard(props) {
     );
 }
 
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+        name : PropTypes.string.isRequired,
+        imgSrc : PropTypes.string.isRequired,
+    }).isRequired,
+}
+
+function App() {
+    const pokemon = {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    };
+
+    return <PokemonCard pokemon={pokemon} />;
+}
 export default PokemonCard;
 
 

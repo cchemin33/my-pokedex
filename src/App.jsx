2baa4db47f127c72bc9cd04bc23838754353077f
cedric chemin <cedric.chemin33@gmail.com>
2023-12-13 16:53:07 +0100

    quete 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
+import PokemonCard from './components/PokemonCard.jsx'
 import { useState } from 'react'
-import PokemonCard from "./components/PokemonCard.jsx"
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import pokemonList from './components/PokemonCard.jsx'
+
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
   const pokemonList = [
     {
       name: "bulbasaur",
@@ -16,18 +15,51 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
   ];
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleNextClick = () => {
+    if (pokemonIndex < pokemonList.length -1){
+    setPokemonIndex(pokemonIndex + 1)
+  }};
+
+  const handlePrevClick = () => {
+    if (pokemonIndex > 0){
+    setPokemonIndex(pokemonIndex - 1)
+  }};
+
+  
+  
   return (
   
   <>
       <div>
-        <PokemonCard pokemonList={pokemonList}/>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+        <button onClick={handlePrevClick}>Précédent</button>
+        <button onClick={handleNextClick}>Suivant</button>
+        
       </div>
   </>
   );
-}
+  }
 
 export default App;
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,36 +1,27 @@
 import PropTypes from "prop-types";
 
-function PokemonCard(props) {
-    console.log(props);
 
-    const pokemon = props.pokemonList[0];
+function PokemonCard({pokemon}) {
+    console.log({pokemon});
 
     return (
         <figure>
-        {pokemon.imgSrc != null ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)
-    }
+        {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
     <figcaption>{pokemon.name}</figcaption>
     </figure >
-    
     );
 }
 
 PokemonCard.propTypes = {
     pokemon: PropTypes.shape({
         name : PropTypes.string.isRequired,
-        imgSrc : PropTypes.string.isRequired,
+        imgSrc : PropTypes.string,
     }).isRequired,
 }
 
-function App() {
-    const pokemon = {
-        name: "bulbasaur",
-        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    };
-
-    return <PokemonCard pokemon={pokemon} />;
-}
 export default PokemonCard;
+
+
 
 
 
